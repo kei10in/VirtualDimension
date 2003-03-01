b94915c3433d774bccfdfec048669838ce11c52c
typz <typz>
2003-03-01 16:31:09 +0000

    Initial release. Provide the functionnalities needed to support the windows (showing, hiding...)

---
+++ Window.h
@@ -0,0 +1,46 @@
+/* 
+ * Virtual Dimension -  a free, fast, and feature-full virtual desktop manager 
+ * for the Microsoft Windows platform.
+ * Copyright (C) 2003 Francois Ferrand
+ *
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation; either version 2 of the License, or (at your option) any later 
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple 
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#ifndef __WINDOW_H__
+#define __WINDOW_H__
+
+#include "desktop.h"
+
+class Window
+{
+public:
+   Window(HWND hWnd);
+   ~Window(void);
+
+   void MoveToDesktop(Desktop * desk);
+   void ShowWindow();
+   void HideWindow();
+   bool IsOnDesk(Desktop * desk) { return desk == m_desk; }
+   operator HWND()               { return m_hWnd; }
+
+   bool m_hiding;
+   bool m_showing;
+
+protected:
+   HWND m_hWnd;
+   Desktop * m_desk;
+};
+
+#endif /*__WINDOW_H__*/
