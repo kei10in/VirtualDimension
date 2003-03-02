89fa0eecb256c31591fd125a1e31390add3bb885
typz <typz>
2003-03-02 15:41:10 +0000

    Add GetIcon, IsHidden, and IsOnDesk APIs. Initial support for hidding methods. Add ITaskbarList interface definition (not included in Mingw/Win32API).

--- Window.h
+++ Window.h
@@ -22,6 +22,29 @@
 #define __WINDOW_H__
 
 #include "desktop.h"
+#include "shlobj.h"
+
+#ifdef __GNUC__
+
+extern "C" const GUID CLSID_TaskbarList;
+extern "C" const GUID IID_ITaskbarList;
+
+#undef INTERFACE
+#define INTERFACE ITaskbarList
+DECLARE_INTERFACE_(ITaskbarList, IUnknown)
+{
+	STDMETHOD(QueryInterface)(THIS_ REFIID,PVOID*) PURE;
+	STDMETHOD_(ULONG,AddRef)(THIS) PURE;
+	STDMETHOD_(ULONG,Release)(THIS) PURE;
+
+   STDMETHOD(ActivateTab)(THIS_ HWND) PURE;
+   STDMETHOD(AddTab)(THIS_ HWND) PURE;
+   STDMETHOD(DeleteTab)(THIS_ HWND) PURE;
+   STDMETHOD(HrInit)(THIS) PURE;
+   STDMETHOD(SetActiveAtl)(THIS_ HWND) PURE;
+};
+
+#endif /*__GNUC__*/
 
 class Window
 {
@@ -30,17 +53,29 @@ public:
    ~Window(void);
 
    void MoveToDesktop(Desktop * desk);
+   bool IsOnDesk(Desktop * desk) { return desk == m_desk; }
+
    void ShowWindow();
    void HideWindow();
-   bool IsOnDesk(Desktop * desk) { return desk == m_desk; }
-   operator HWND()               { return m_hWnd; }
+   bool IsHidden() const         { return m_hidden; }
 
-   bool m_hiding;
-   bool m_showing;
+   operator HWND()               { return m_hWnd; }
+   HICON GetIcon(void);
+
+   enum HidingMethods {
+      WHM_HIDE,
+      WHM_MINIMIZE, 
+      WHM_MOVE
+   };
 
 protected:
    HWND m_hWnd;
    Desktop * m_desk;
+   bool m_hidden;
+   int m_hidingMethod;
+   bool m_iconic;
+
+   static ITaskbarList* m_tasklist;
 };
 
 #endif /*__WINDOW_H__*/
