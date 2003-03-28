dd668e0adfe7dfc47b2717fc112ad788d59c4a79
typz <typz>
2003-03-28 00:40:56 +0000

    Add a resource id for the new "enable tooltips" configuration checkbox in the general configuration sheet.

--- Resource.h
+++ Resource.h
@@ -65,6 +65,7 @@
 #define IDC_TRANSP_STATIC1              1032
 #define IDC_TRANSP_STATIC2              1033
 #define IDC_TRANSP_STATIC               1034
+#define IDC_TOOLTIPS_CHECK              1035
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
