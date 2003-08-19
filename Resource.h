a3c4d49b371ec607db2fe086a955f85b160483b9
typz <typz>
2003-08-19 12:05:52 +0000

    Add color selection to the desktop properties dialog.

--- Resource.h
+++ Resource.h
@@ -112,7 +112,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        142
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1070
+#define _APS_NEXT_CONTROL_VALUE         1071
 #define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
