93259c10574817a9aa3deb55578165e7a697dd8a
typz <typz>
2003-10-23 16:44:03 +0000

    Add shortcuts configuration property page.

--- Resource.h
+++ Resource.h
@@ -50,6 +50,7 @@
 #define IDD_WINDOW_AUTOSETTINGS         139
 #define IDD_OSD_SETTINGS                140
 #define IDD_DISPLAY_SETTINGS            141
+#define IDD_SHORTCUT_SETTINGS           142
 #define IDC_DESK_LIST                   1001
 #define IDC_INSERT_DESK                 1002
 #define IDC_REMOVE_DESK                 1003
@@ -105,6 +106,7 @@
 #define IDC_EXTRAPARAM_BTN              1068
 #define IDC_GPL_LINK                    1072
 #define IDC_HOMEPAGE_LINK               1075
+#define IDC_SHORTCUTSLIST               1076
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -112,9 +114,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_RESOURCE_VALUE        143
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1076
+#define _APS_NEXT_CONTROL_VALUE         1077
 #define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
