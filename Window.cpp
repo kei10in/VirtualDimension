bcf40c18fb12c147bbd23df9ed8bc9dfc2460489
typz <typz>
2004-01-18 21:21:06 +0000

    When hiding the window, do not let it know its frame has changed.

--- Window.cpp
+++ Window.cpp
@@ -257,7 +257,7 @@ void Window::HideWindow()
    {
       LONG_PTR style = (m_style & ~WS_EX_APPWINDOW) | WS_EX_TOOLWINDOW;
       SetWindowLongPtr(m_hWnd, GWL_EXSTYLE, style);
-      SetWindowPos(m_hWnd, NULL, 0, 0, 0, 0, SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+      SetWindowPos(m_hWnd, NULL, 0, 0, 0, 0, SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
    }
    
    m_hidden = true;
